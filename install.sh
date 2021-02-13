c8b0dc2c5e51ba7b1ea869e15cfde164484d6b32
ayush saraf <ayushsaraf.saraf15@gmail.com>
2021-02-13 10:30:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
+sudo apt -y update
+
+sudo apt -y install nodejs
+
+sudo apt -y install npm
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt -y update
+
+sudo apt install -y mongodb-org
+
+sudo systemctl start mongod
+
+sudo systemctl enable mongod
+
 npm init -y
 
 cd src
