7a751a9c9056311ab9ec9ffda2428e4c8e3be6ed
ayush saraf <55061546+ayush2401@users.noreply.github.com>
2021-02-17 18:20:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 
 sudo apt -y install nodejs
 
-sudo apt -y install npm
-
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
@@ -17,6 +15,8 @@ sudo apt install -y mongodb-org
 sudo systemctl start mongod
 
 sudo systemctl enable mongod
+
+mongo node-app --eval "db.dropDatabase()"
 
 npm init -y
 
